a4396508eec6e320a785b704324a61bfa2406957
Metroynome <yort124@gmail.com>
2026-02-25 17:44:46 -0500

    moby + player header files added

---
+++ include/moby.h
@@ -0,0 +1,57 @@
+#ifndef _LIBGC_MOBY_H_
+#define _LIBGC_MOBY_H_
+
+#include <tamtypes.h>
+#include "types.h"
+#include "math3d.h"
+
+enum MobyIds {
+    MOBY_ID_ASDF = 0,
+};
+
+typedef struct Moby { // 0x100
+/* 0x000 */ VECTOR bSphere;
+/* 0x010 */ VECTOR position;
+/* 0x020 */ char state;
+/* 0x021 */ u8 group;
+/* 0x022 */ char mClass;
+/* 0x023 */ u8 opacity;
+/* 0x024 */ struct MobyClass* pClass;
+/* 0x028 */ struct Moby * pChain;
+/* 0x02c */ float scale;
+} Moby;
+
+struct MobyClass { // 0x50
+/* 0x00 */ void *packets;
+/* 0x04 */ char packet_cnt_0;
+/* 0x05 */ char packet_cnt_1;
+/* 0x06 */ char metal_cnt;
+/* 0x07 */ char metal_ofs;
+/* 0x08 */ char joint_cnt;
+/* 0x09 */ char pad;
+/* 0x0a */ char packet_cnt_2;
+/* 0x0b */ char team_texs;
+/* 0x0c */ char seq_cnt;
+/* 0x0d */ char sound_cnt;
+/* 0x0e */ char lod_trans;
+/* 0x0f */ char shadow;
+/* 0x10 */ short int *collision;
+/* 0x14 */ void *skeleton;
+/* 0x18 */ void *common_trans;
+/* 0x1c */ void *anim_joints;
+/* 0x20 */ void *gif_usage;
+/* 0x24 */ float gScale;
+/* 0x28 */ struct SoundDef *sound_defs;
+/* 0x2c */ char bangles;
+/* 0x2d */ char mip_dist;
+/* 0x2e */ short int corncob;
+/* 0x30 */ VECTOR bSphere;
+/* 0x40 */ int glow_rgba;
+/* 0x44 */ short int mode_bits;
+/* 0x46 */ char type;
+/* 0x47 */ char mode_bits2;
+/* 0x48 */ void *seqs[0];
+};
+
+
+#endif
