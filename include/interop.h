725dcca80c15f018a8f86ed5fb3a59ec794f778b
Metroynome <yort124@gmail.com>
2026-03-16 12:35:02 -0400

    updated variableaddress fields

--- include/interop.h
+++ include/interop.h
@@ -12,17 +12,33 @@
 #include <tamtypes.h>
 
 typedef struct VariableAddress {
-  u32 Lobby;
-  u32 Bakisi;
-  u32 Hoven;
-  u32 OutpostX12;
-  u32 KorgonOutpost;
-  u32 Metropolis;
-  u32 BlackwaterCity;
-  u32 CommandCenter;
-  u32 BlackwaterDocks;
-  u32 AquatosSewers;
-  u32 MarcadiaPalace;
+	u32 Aranos1;
+	u32 Oozla;
+	u32 Maktar;
+	u32 Endako;
+	u32 Barlow;
+	u32 Feltzino;
+	u32 Notak;
+	u32 Siberius;
+	u32 Tabora;
+	u32 Dobbo;
+	u32 Hrugis;
+	u32 Joba;
+	u32 Todano;
+	u32 Boldan;
+	u32 Aranos2;
+	u32 Gorn;
+	u32 Snivelak;
+	u32 Smolg;
+	u32 Damosel;
+	u32 Grelbin;
+	u32 Yeedil;
+	u32 DobboOrbit;
+	u32 DamoselOrbit;
+	u32 ShipShack;
+	u32 Wupash;
+	u32 JammingArray;
+	u32 Museum;
 } VariableAddress_t;
 
 u32 GetAddress(VariableAddress_t* address);
